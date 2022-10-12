52c4b87d038213a5a54bf45db5a524fe0d79c525
Roberto Carlos Callisaya <rccallisaya@grupobisa.com>
2022-10-11 20:33:47 -0400

    fixed viewmodel errors

--- app/src/main/java/com/example/viewmodelapp/MainActivity.kt
+++ app/src/main/java/com/example/viewmodelapp/MainActivity.kt
@@ -2,39 +2,19 @@ package com.example.viewmodelapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.TextView
-import androidx.activity.viewModels
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import com.google.gson.Gson
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+
 
 class MainActivity : AppCompatActivity() {
-    private val myTextView: TextView get() = findViewById(R.id.myTextView)
+//    private val myTextView: TextView get() = findViewById(R.id.myTextView)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val myViewModel: MyViewModel by viewModels()
+//        val myViewModel: MyViewModel by viewModels()
+//
+//        myViewModel.myLiveData.observe(this) {
+//            text -> myTextView.text = text
+//        }
 
-        myViewModel.myLiveData.observe(this) {
-            text -> myTextView.text = text
-        }
-        val restApiAdapter = RestApiAdapter()
-        val endPoint = restApiAdapter.connectionApi()
-
-        val bookResponseCall = endPoint.getAllPost()
-        bookResponseCall.enqueue( object : Callback<List<Post>> {
-            override fun onFailure(call: Call<List<Post>>, t: Throwable) {
-                t?.printStackTrace()
-            }
-
-            override fun onResponse(call: Call<List<Post>>, response: Response<List<Post>>) {
-                Log.d("",Gson().toJson(response.body()))
-            }
-        })
 
 
 
--- app/src/main/java/com/example/viewmodelapp/NegociosActivity.kt
+++ app/src/main/java/com/example/viewmodelapp/NegociosActivity.kt
@@ -2,13 +2,38 @@ package com.example.viewmodelapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
+import com.google.gson.Gson
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class NegociosActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_negocios)
+
+        val restApiAdapter = RestApiAdapter()
+        val endPoint = restApiAdapter.connectionApi()
+
+        val bookResponseCall = endPoint.getAllPost()
+        bookResponseCall.enqueue( object : Callback<List<Post>> {
+            override fun onFailure(call: Call<List<Post>>, t: Throwable) {
+                t?.printStackTrace()
+            }
+
+            override fun onResponse(call: Call<List<Post>>, response: Response<List<Post>>) {
+                Log.d("", Gson().toJson(response.body()))
+                val posts = response?.body()
+                Log.d("RESP POST", Gson().toJson(posts))
+                posts?.forEach {
+                    Log.d("RESP POST", it.body)
+                }
+
+            }
+        })
     }
 
     fun selectedButton(view: View) {
